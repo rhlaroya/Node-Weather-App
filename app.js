f429e653298df13c6f3644a2baf5ab60272e00b4
Regae Laroya <regae.laroya@collabera.com>
2022-01-15 18:00:24 -0500

    Performed callback chaining

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Providence', (error, data) => {
-    console.log('Error', error);
-    console.log('Data', data);
-});
+const address = process.argv[2]
 
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
+if(!address){
+    console.log('Please provide an address')
+} else {
+    geocode(address, (error, data) => {
+
+        if(error) {
+            return console.error(error)
+        }
+        
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.error(error)
+            }
+    
+            console.log(data.location)
+            console.log(forecastData)
+    
+        })
+    });
+}
+
 
