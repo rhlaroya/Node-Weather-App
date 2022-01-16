035a9d1738d7b3b814c737b84e1841c7f5245f07
Regae Laroya <regae.laroya@collabera.com>
2022-01-16 15:01:04 -0500

    Performed destructuring and object property shorthand

--- utils/forecast.js
+++ utils/forecast.js
@@ -2,29 +2,15 @@ const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=5324bc607f6dd873a80032a6887dc470&query=' + latitude +','+ longitude + '&units=f';
-    request({ url: url, json: true}, (error, response) => {
+    request({ url, json: true}, (error, { body }) => {
         if(error){
             callback('Unable to connect to weatherstack services', undefined);
-        } else if(response.body.error){
+        } else if(body.error){
             callback('Unable to get forecast try another search', undefined);
         } else {
-            callback(undefined, response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degress out.")
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out.")
         }
     })
 }
 
 module.exports = forecast
-
-// request({ url: url, json: true }, (error, response) => {
-//     console.log(response.body.current)
-// })
-
-// request({ url: url, json: true }, (error, response) => {
-//     if(error){
-//         console.log(error);
-//     } else if (response.body.error){
-//         console.log(response.body.error)
-//     } else {
-//         console.log("It is currently " + response.body.current.temperature + " degrees out. There is " + response.body.current.precip + "% chance or rain");
-//     }
-// });
--- utils/geocode.js
+++ utils/geocode.js
@@ -2,16 +2,16 @@ const request = require('request')
 
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoicmhsYXJveWEiLCJhIjoiY2t5ZGdnamVqMHoyZTJ1cWtwZjdldnl0MSJ9.MjoMJGru7jEaTko9x0YTtw';
-    request({ url: url, json: true}, (error, response) =>{
+    request({ url, json: true}, (error, { body }) =>{
         if(error){
             callback('Unable to connect to location services')
-        } else if(response.body.features.length === 0){
+        } else if(body.features.length === 0){
             callback('Unable to find location try another search', undefined)
         } else {
             callback(undefined, {
-                latitude: response.body.features[0].center[1],
-                longitude: response.body.features[0].center[0],
-                location: response.body.features[0].place_name
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
             })
         }
     })
